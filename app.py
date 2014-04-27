bd3032bfe0da84fa313e1cbf41d3fb344bbaa467
Danny Rosen <danny.rosen@gmail.com>
2014-04-27 12:18:14 -0400

    Initial

---
+++ app.py
@@ -0,0 +1,26 @@
+import json
+import bottle
+from bson import json_util
+from bottle import route, run, request, abort, response
+from pymongo import Connection
+ 
+connection = Connection('localhost', 27017)
+db = connection.mydatabase
+
+@route('/action', method='POST')
+def add_action():
+    data = request.body.readline()
+    if not data:
+        abort(400, 'You gave me nothing. You fail.')
+    entity = json.loads(data)
+    print entity
+    if not entity.has_key('actionName'):
+        abort(400, 'I need an action')
+    if not entity.has_key('time'):
+        abort(400, 'I need a time')
+    try:
+        db['action'].save(entity)
+    except ValidationError as valerr:
+        abort(400, str(valerr))
+
+run(host='localhost', port=8080)
