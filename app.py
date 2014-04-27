2ec4c462edd2edaade5302020607ae2ba02240b6
Danny Rosen <danny.rosen@gmail.com>
2014-04-27 17:52:13 -0400

    for the win

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import bottle
 from bottle import route, run, request, abort, response
 from pymongo import Connection
+from time import time
  
 connection = Connection('localhost', 27017)
 db = connection.actions
@@ -9,14 +10,18 @@ db = connection.actions
 @route('/action', method='POST')
 def add_action():
     data = request.body.readline()
+    print type(data)
+    print data
     if not data:
         abort(400, 'You gave me nothing. You fail.')
     entity = json.loads(data)
-    print entity
+    print type(entity)
+    entity['time']= time()
+    print entity['actionName']
     if not entity.has_key('actionName'):
         abort(400, 'I need an action')
     try:
-        db['action'].save(entity)
+        db["action"].update({ "action":entity['actionName'] },{ "$addToSet":{"time":time()} },upsert=True)
     except ValidationError as valerr:
         abort(400, str(valerr))
 
