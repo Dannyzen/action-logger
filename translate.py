09e31674a693e596692a091f3c2dd28b0ea66fc5
Danny Rosen <danny.rosen@gmail.com>
2014-05-06 15:10:43 -0400

    little better

--- translate.py
+++ translate.py
@@ -1,4 +1,11 @@
-import time
-time_list = []
-for item in time_list:
-    print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(item))
+from sys import argv
+from time import localtime, strftime
+
+times = [1399402955.584515]
+
+def printTimes(times):
+    for item in times:
+        print strftime('%Y-%m-%d %H:%M:%S', localtime(item))
+
+if __name__ == "__main__":
+    printTimes(times)
