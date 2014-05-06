b344d6f96d37a157765c1a74ce83a84f0d87b07e
Danny Rosen <danny.rosen@gmail.com>
2014-05-06 18:15:54 -0400

    added help, parameter based actions and refactored for pretty

--- translate.py
+++ translate.py
@@ -1,11 +1,29 @@
 from sys import argv
 from time import localtime, strftime
+from pymongo import Connection
 
-times = [1399402955.584515]
+connection = Connection('localhost', 27017)
+db = connection.actions
 
-def printTimes(times):
-    for item in times:
-        print strftime('%Y-%m-%d %H:%M:%S', localtime(item))
+#action_name = "enter_car"
+
+def showActions():
+        actions = db["action"].find({"action":{"$exists":True}},{"action":1})
+        for item in actions:
+                print item['action']
+
+def showTimes(action_name):
+        entries = returnEntries(action_name)
+        for item in entries['time']:
+                print strftime('%Y-%m-%d %H:%M:%S', localtime(item))
+
+def returnEntries(action_name):
+        entries = db["action"].find_one({"action":action_name})
+        return entries
+
 
 if __name__ == "__main__":
-    printTimes(times)
+        if argv[1] == "help":
+                showActions()
+        else:
+                showTimes(argv[1])
